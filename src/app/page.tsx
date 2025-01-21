538b34c0d073116b4ea83c5109f5e0791df518c8
Dmitry Kanishchev <dmitry.kanishchev@benefactory.ru>
2025-01-21 21:23:51 +0400

    feat: Navbar added
    Created Navbar for desktop and mobile, added sidebar on page and styles for it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,8 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>
-            Sign in
-          </Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
+      Home page
     </div>
   );
 }
