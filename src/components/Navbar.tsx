164e0e40e50ac13481851b24e04fb9e705918d69
Dmitry Kanishchev <dmitry.kanishchev@benefactory.ru>
2025-04-27 14:13:29 +0400

    feat: :lock: Auth and db sync
    Sync auth with db, update Next.js to 15.3.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,22 @@
-import Link from 'next/link'
-import React from 'react'
-import DesktopNavbar from './DesktopNavbar'
-import MobileNavbar from './MobileNavbar'
+import Link from 'next/link';
+import React from 'react';
+import DesktopNavbar from './DesktopNavbar';
+import MobileNavbar from './MobileNavbar';
+import { currentUser } from '@clerk/nextjs/server';
+import { syncUser } from '@/actions/user.action';
 
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser();
   return (
     <nav className='sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50'>
-      <div className="max-w-7xl mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center'>
+            <Link
+              href='/'
+              className='text-xl font-bold text-primary font-mono tracking-wider'
+            >
               Socially
             </Link>
           </div>
@@ -19,7 +26,7 @@ function Navbar() {
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
